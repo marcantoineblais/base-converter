e70167c06cafcd8bad571a948b41559abce51756
Marc Blais <blaisma@live.fr>
2023-03-03 11:29:09 -0500

    need to work on toBaseN problems

--- BaseConv.java
+++ BaseConv.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public final class BaseConv {
   public static void main(String[] args) {
     Scanner input;
-    int base;
-    String userNumber;
+    int startBase;
+    String userNumber, baseTenValue;
 
     input = new Scanner(System.in);
     display("Enter base to convert from : ");
-    base = input.nextInt();
+    startBase = input.nextInt();
     display("Enter a number : ");
     userNumber = input.next();
     input.close();
-    String newValue = toBaseTen(userNumber, base);
-    display("Your number in base 10 is : " + newValue);
+    baseTenValue = toBaseTen(userNumber, startBase);
+    for (int base = 2; base <= 16; base ++) {
+      display("Your number in base " + base + " is : " + toBaseN(baseTenValue, base));
+    }
   }
 
   public static void display(String text) {
@@ -32,24 +34,10 @@ public final class BaseConv {
     System.out.println(num);
   }
 
-  public static double power(int n, int exp) {
-    double result = 1;
-    if (exp > 0) {
-      for (int i = 0; i < exp; i ++) {
-        result *= n;
-      }
-    } else if (exp < 0) {
-      for (int i = 0; i < -exp; i++) {
-        result *= n;
-      }
-      result = 1 / result;
-    }
-    return result;
-  }
-
   public static int hexToNum(char c) {
     char[] letters = {'a', 'b', 'c', 'd', 'e', 'f'};
     int value;
+    
     for (int i = 0; i < letters.length; i++) {
       if (c == letters[i]) {
         value = i + 10;
@@ -63,6 +51,7 @@ public final class BaseConv {
   public static char numToHex(int value) {
     char[] letters = {'a', 'b', 'c', 'd', 'e', 'f'};
     char c;
+    
     if (value >= 10) {
       c = letters[value - 10];
     } else {
@@ -78,9 +67,10 @@ public final class BaseConv {
     int numLength = numArray.length, decLength;
     double value = 0;
     double subValue;
+    
     for (int i = 0; i < numLength; i++) {
       char n = numArray[numLength - i - 1];
-      subValue = power(base, i) * hexToNum(n);
+      subValue = Math.pow(base, i) * hexToNum(n);
       value += subValue;
     }
 
@@ -90,10 +80,57 @@ public final class BaseConv {
       decLength = decArray.length;
       for (int i = 0; i < decLength; i++) {
         char n = decArray[i];
-        subValue = power(base, -(i + 1)) * hexToNum(n);
+        subValue = Math.pow(base, -(i + 1)) * hexToNum(n);
         value += subValue;
       }
     }
     return Double.toString(value);
+  }
+
+  public static String toBaseN(String fullNumber, int base) {
+    String[] splitNumber = fullNumber.split("\\.", 2);
+    String newNumber = "";
+    int num = Integer.parseInt(splitNumber[0]);
+    int exp = 0;
+    int subTotal = 0;
+
+    while (Math.pow(base, exp + 1) <= num) {
+      exp ++;
+    }
+
+    for (int e = exp; e >= 0; e --) {
+      int positionValue = (int) Math.pow(base, e);
+      int n = 0;
+
+      while (subTotal + positionValue <= num) {
+        subTotal += positionValue;
+        n ++;
+      }
+      newNumber += Integer.toString(n);
+    }
+
+    if (splitNumber.length > 1) {
+      double dec;
+      String decStr = "0." + splitNumber[1];
+      
+      dec = Double.parseDouble(decStr);
+      newNumber += ".";
+      subTotal = 0;
+
+      for (int e = -1; e >= -16; e --) {
+        double positionValue = Math.pow(base, e);
+        int n = 0;
+
+        while (subTotal + positionValue <= num) {
+          subTotal += positionValue;
+          n ++;
+        }
+        newNumber += n;
+        if (subTotal == dec);
+          break;
+      }
+    }
+
+    return newNumber;
   }
 }
